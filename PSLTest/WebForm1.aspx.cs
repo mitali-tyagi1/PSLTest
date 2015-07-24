55da8ecaab9a6f4e7c6216f780e686c782eaf932
Mohit Singh Negi <mohit.negi@hcl.com>
2015-07-24 18:20:53 +0530

    Vineet Code merge.

--- PSLTest/WebForm1.aspx.cs
+++ PSLTest/WebForm1.aspx.cs
@@ -22,23 +22,28 @@ namespace PSLTest
             {
                 pubrequest info = (pubrequest)serializer.Deserialize(reader);
                 var data = info.Request.Select(m => m.Toc.Structure);
-                
+
                 r.Item = new List<Item>();
                 var item = Mapper.Map<List<request>, List<Item>>(info.Request);
                 r.Item = item;
                 var text = Mapper.Map<List<Structure>, List<Data>>(data.ToList());
-                var toc = Mapper.Map<List<Structure>, List<BfwTocContents>>(data.Where(m => m != null).ToList());   
+                var toc = Mapper.Map<List<Structure>, List<BfwTocContents>>(data.Where(m => m != null).ToList());
                 var title = Mapper.Map<List<ContentMetaData>, List<Data>>((info.Request.Select(m => m.ContentMetaData)).Where(m => m != null).ToList());
-                text.ForEach(m => r.Item.ForEach(x => x.Data = m));                
+                var type = Mapper.Map<List<request>, List<Data>>(info.Request.Where(m => m != null).ToList());
+
+                text.ForEach(m => r.Item.ForEach(x => x.Data = m));
                 title.ForEach(m => r.Item.ForEach(x => x.Data.Title = m.Title));
+                toc.ForEach(m => r.Item.ForEach(x => ((x.Data.BfwTocs = new BfwTocs()).BfwTocContents = new BfwTocContents()).ParentId = m.ParentId));
                 toc.ForEach(m => r.Item.ForEach(x => ((x.Data.BfwTocs = new BfwTocs()).BfwTocContents = new BfwTocContents()).Sequence = m.Sequence));
+                type.ForEach(m => r.Item.ForEach(x => x.Data = m));
+
             }
             var serialize = new XmlSerializer(typeof(ItemRequest));
 
             using (var writer = File.OpenWrite("D:\\GitHub\\PSL\\PSLTest\\PSLTest\\PSLTest\\FinalXml.xml"))
             {
                 serialize.Serialize(writer, r);
-            }      
+            }
         }
     }
 }
